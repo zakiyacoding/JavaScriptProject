8fc38b642664c2fa11d260a6caf08e15b2c8799b
Zakiya coding <z4zakiya@gmail.com>
2025-12-26 23:45:33 +0530

    new

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jones");
+console.log(23);
+
+let firstName = "Matilda";
