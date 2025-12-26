cfbf7d8e27746e612b3d3a7c6e0d699ef6d8a6d9
Zakiya coding <z4zakiya@gmail.com>
2025-12-26 17:52:06 +0530

    add

---
+++ script.js
@@ -0,0 +1,13 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let 3years = 3;
