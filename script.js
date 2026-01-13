1b87ed9a2ef881acd46f81173c06aa757745b046
Zakiya coding <z4zakiya@gmail.com>
2026-01-13 18:15:23 +0530

    new

--- script.js
+++ script.js
@@ -60,12 +60,10 @@ job = "teacher";
 lastName = "Schmedtmann";
 console.log(lastName);
 
-*/
-
 // Math operators
 const now = 2037;
 const ageJonas = now - 1991;
-const ageSarah = now - 2020;
+const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
 
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
@@ -89,3 +87,19 @@ console.log(ageJonas > ageSarah); // >, <, >=, <=
 console.log(ageSarah >= 18);
 
 const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
