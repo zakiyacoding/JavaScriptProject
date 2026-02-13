a33e8d2f65d1f776196e04a12f65fe1fb8987776
Zakiya coding <z4zakiya@gmail.com>
2026-02-13 09:38:35 +0530

    add

--- script.js
+++ script.js
@@ -135,14 +135,13 @@ if (age >= 18) {
   console.log("Sarah can start driving license 🚗");
 } else {
   const yearsLeft = 18 - age;
-  console.log(`Sarah is too young. Wait another $
-    {yearsLeft} years :)`);
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
 }
 
 const birthYear = 1991;
 if (birthYear <= 2000) {
-  let century = 20;
+  century = 20;
 } else {
-  let century = 21;
+  century = 21;
 }
 console.log(century);
