2db54ce79f6bdf187a37d57f14e542cdbb7d1939
Zakiya coding <z4zakiya@gmail.com>
2026-02-20 12:32:07 +0530

    add

--- script.js
+++ script.js
@@ -147,12 +147,17 @@ if (birthYear <= 2000) {
 console.log(century);
 */
 
+const massMark = 78;
+const heightmark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
 const massMark = 95;
 const heightmark = 1.88;
 const massJohn = 85;
 const heightJohn = 1.76;
 
-const BMIMark = massMark / heightMark ** 2;
+const BMIMark = massMark / heightmark ** 2;
 const BMIJohn = massJohn / (heightJohn * heightJohn);
 console.log(BMIMark, BMIJohn);
 
